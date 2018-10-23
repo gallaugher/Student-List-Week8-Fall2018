c10860ef77201142a1f718202c590edeacaadd5e
John Gallaugher <john.gallaugher@bc.edu>
2018-10-23 11:39:18 -0400

    Exercise after Week 8 - selected student name is passed to new view controller, or nothing is in the student name if "+" is shown. Cancel works, but no passing back saved or edited student name at this point.

--- Student List/ViewController.swift
+++ Student List/ViewController.swift
@@ -9,12 +9,64 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    
+    var students = ["Subraiz Ahmed",
+                    "Jack Antico",
+                    "Carter Beaulieu",
+                    "Michael Burke",
+                    "Stepan Cannuscio",
+                    "Paige Carey",
+                    "James Clark",
+                    "Matthew Donovan",
+                    "Caroline Downey",
+                    "Anran Du",
+                    "Griffin Elliott",
+                    "Alexandro Forte",
+                    "Dawson Gallay",
+                    "Jacqueline Han",
+                    "Joseph Langenderfer",
+                    "Lok Lee",
+                    "Yujiao Li",
+                    "Emma Loughlin",
+                    "Jolene Lozano",
+                    "Jack Manoog",
+                    "Benjamin Meisenzahl",
+                    "Dylan Nadeau",
+                    "Brennan Nugent",
+                    "Nicholas OBrien Cannon",
+                    "Jessica Olivieri",
+                    "Peter Song",
+                    "Rahul Tasker",
+                    "John Wilson"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
-
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowStudent" {
+            let destination = segue.destination as! StudentDetailViewController
+            let selectedIndexPath = tableView.indexPathForSelectedRow!
+            destination.student = students[selectedIndexPath.row]
+        }
+    }
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // print("👊 numberOfRowsInSection was just called and there are \(students.count) rows in the tableView.")
+        return students.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // print("👍👍 Dequeing the table view cell for indextPath.row = \(indexPath.row) where the cell contaibns item \(students[indexPath.row])")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = students[indexPath.row]
+        return cell
+    }
+    
+}
